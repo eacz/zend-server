e0c599c51ef90c474af682bf987a41271bdef8d4
Esteban <estebanacanteros@gmail.com>
2020-08-23 19:55:46 -0300

    changes for production

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const corsConfig = {
 };
 app.use(cors(corsConfig));
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+    );
+    next();
+});
+
 connectDB();
 
 //enable read data through body
